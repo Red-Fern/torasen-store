7d6f8292c8e388cb8cce0ee4be24fb91b6d230af
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2024-05-02 10:07:58 +0100

    Create Torasen Store plugin for custom blocks

---
+++ web/app/plugins/torasenstore/torasenstore.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name:       Torasen Store
+ * Description:       Custom functionality for Torasen Store.
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            Red Fern
+ * Text Domain:       torasenstore
+ *
+ * @package torasenstore
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function create_block_torasenstore_block_init() {
+	$blocks = [
+
+	];
+
+	foreach ($blocks as $block) {
+		register_block_type( __DIR__ . "/build/{$block}" );
+	}
+}
+add_action( 'init', 'create_block_torasenstore_block_init' );
