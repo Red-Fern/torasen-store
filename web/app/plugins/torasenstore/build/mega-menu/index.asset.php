8e8f6a8531a76b235c5e68b66f17d25d693dbbae
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2024-05-02 10:08:50 +0100

    Scaffold mega menu block

---
+++ web/app/plugins/torasenstore/build/mega-menu/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '6c24bce33eb5e3f614cb');
---
+++ web/app/plugins/torasenstore/build/mega-menu/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '66e31ab901c988b7da4c');
