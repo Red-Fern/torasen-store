4fee9359dcfcdab06d047efcb7f5461c60399318
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2024-06-24 17:02:03 +0100

    Reformat swiper init

--- web/app/themes/torasenstore/resources/js/swiper.js
+++ web/app/themes/torasenstore/resources/js/swiper.js
@@ -1,50 +1,36 @@
 import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 import 'swiper/css';
 
-(function () {
-    let carouselElements = document.querySelectorAll('.swiper-container');
+document.querySelectorAll('.swiper-container').forEach((el) => {
+    // Get carousel, arrows and data attributes
+    let carousel = el.querySelector('.swiper'),
+        prevArrow = el.querySelector('.swiper-button-prev'),
+        nextArrow = el.querySelector('.swiper-button-next'),
+        noSlides = carousel.dataset.slides ?? 1,
+        noSlidesSm = carousel.dataset.slidesSm ?? noSlides,
+        noSlidesMd = carousel.dataset.slidesMd ?? noSlides,
+        noSlidesLg= carousel.dataset.slidesLg ?? noSlides,
+        slidesGap = carousel.dataset.slidesGap ?? 20;
 
-    function initCarousels() {
-        for (let i = 0; i < carouselElements.length; i++) {
-            // Get carousel, arrows and data attributes
-            let carousel = carouselElements[i].querySelector('.swiper'),
-                prevArrow = carouselElements[i].querySelector('.swiper-button-prev'),
-                nextArrow = carouselElements[i].querySelector('.swiper-button-next'),
-                noSlides = carousel.dataset.slides ?? 1,
-                noSlidesSm = carousel.dataset.slidesSm ?? noSlides,
-                noSlidesMd = carousel.dataset.slidesMd ?? noSlides,
-                noSlidesLg= carousel.dataset.slidesLg ?? noSlides,
-                slidesGap = carousel.dataset.slidesGap ?? 20;
-
-            new Swiper(carousel, {
-                navigation: {
-                    prevEl: prevArrow,
-                    nextEl: nextArrow
-                },
-                slidesPerView: noSlides,
-                spaceBetween: slidesGap,
-                breakpoints: {
-                    640: {
-                        slidesPerView: noSlidesSm
-                    },
-                    768: {
-                        slidesPerView: noSlidesMd
-                    },
-                    1024: {
-                        slidesPerView: noSlidesLg
-                    }
-                }
-            });
-        }
-    }
-
-    domReady(function () {
-        if (!document.body) {
-            return;
-        } else {
-            if (carouselElements) {
-                initCarousels();
+    new Swiper(carousel, {
+        modules: [Navigation, Pagination],
+        navigation: {
+            prevEl: prevArrow,
+            nextEl: nextArrow
+        },
+        slidesPerView: noSlides,
+        spaceBetween: slidesGap,
+        breakpoints: {
+            640: {
+                slidesPerView: noSlidesSm
+            },
+            768: {
+                slidesPerView: noSlidesMd
+            },
+            1024: {
+                slidesPerView: noSlidesLg
             }
         }
     });
-});
+})
