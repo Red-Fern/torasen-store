e615cf73341a1a6e754d0842c3046e51b1789d30
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2024-06-25 15:08:21 +0100

    Amend swiper to work with data attributes and display in the editor

--- web/app/themes/torasenstore/acf/blocks/hero/hero.php
+++ web/app/themes/torasenstore/acf/blocks/hero/hero.php
@@ -19,29 +19,32 @@
 
 <?php if (have_rows('slides')): ?>
     <div <?php echo esc_attr($anchor); ?> class="<?php echo esc_attr($class_name); ?>">
-        <div class="swiper-container">
-            <div class="swiper">
-                <div class="swiper-wrapper">
-                    <?php foreach(get_field('slides') as $slide): ?>
-                        <?php if ($slide['bg_image']): ?>
-                            <div class="swiper-slide">
-                                <a href="<?php echo $slide['link'] ? $slide['link']['url'] : '#'; ?>" class="block w-full aspect-[393/596] | md:aspect-[1739/837]">
-                                    <picture>
-                                        <?php if ($slide['mob_bg_image']): ?>
-                                            <source media="(max-width: 781px)" srcset="<?php echo $slide['mob_bg_image']['url'] ?>">
-                                            <source media="(min-width: 782px)" srcset="<?php echo $slide['bg_image']['url'] ?>">
-                                        <?php endif; ?>
+        <div class="swiper" data-swiper='{
+            "autoplay": {"delay": 6000},
+            "loop": true
+        }'>
+            <div class="swiper-wrapper">
+                <?php foreach(get_field('slides') as $slide): ?>
+                    <?php if ($slide['bg_image']): ?>
+                        <div class="swiper-slide">
+                            <a href="<?php echo $slide['link'] ? $slide['link']['url'] : '#'; ?>" class="block w-full aspect-[393/596] | md:aspect-[1739/837]">
+                                <picture>
+                                    <?php if ($slide['mob_bg_image']): ?>
+                                        <source media="(max-width: 781px)" srcset="<?php echo $slide['mob_bg_image']['url'] ?>">
+                                        <source media="(min-width: 782px)" srcset="<?php echo $slide['bg_image']['url'] ?>">
+                                    <?php endif; ?>
 
-                                        <img src="<?php echo $slide['bg_image']['url'] ?>" class="w-full h-full object-cover" alt="<?php echo $slide['bg_image']['alt'] ?>">
-                                    </picture>
-                                </a>
-                            </div>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                </div>
+                                    <img src="<?php echo $slide['bg_image']['url'] ?>" class="w-full !h-full object-cover" alt="<?php echo $slide['bg_image']['alt'] ?>">
+                                </picture>
+                            </a>
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
 <?php elseif ($is_preview): ?>
-    Editor
+    <div class="px-root">
+        <div class="p-xs w-full border border-mid-grey text-dark-grey text-center">Slides will be displayed in a scrollable list once added.</div>
+    </div>
 <?php endif; ?>
--- web/app/themes/torasenstore/resources/js/swiper.js
+++ web/app/themes/torasenstore/resources/js/swiper.js
@@ -1,36 +1,27 @@
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from 'swiper/bundle';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 
-document.querySelectorAll('.swiper-container').forEach((el) => {
-    // Get carousel, arrows and data attributes
-    let carousel = el.querySelector('.swiper'),
-        prevArrow = el.querySelector('.swiper-button-prev'),
-        nextArrow = el.querySelector('.swiper-button-next'),
-        noSlides = carousel.dataset.slides ?? 1,
-        noSlidesSm = carousel.dataset.slidesSm ?? noSlides,
-        noSlidesMd = carousel.dataset.slidesMd ?? noSlides,
-        noSlidesLg= carousel.dataset.slidesLg ?? noSlides,
-        slidesGap = carousel.dataset.slidesGap ?? 20;
+let defaults = {
+    modules: [Navigation, Pagination, Autoplay],
+    slidesPerView: 1,
+    spaceBetween: 0,
+    speed: 950
+};
 
-    new Swiper(carousel, {
-        modules: [Navigation, Pagination],
-        navigation: {
-            prevEl: prevArrow,
-            nextEl: nextArrow
-        },
-        slidesPerView: noSlides,
-        spaceBetween: slidesGap,
-        breakpoints: {
-            640: {
-                slidesPerView: noSlidesSm
-            },
-            768: {
-                slidesPerView: noSlidesMd
-            },
-            1024: {
-                slidesPerView: noSlidesLg
-            }
-        }
+initSwipers(defaults);
+
+function initSwipers(defaults = {}, selector = ".swiper") {
+    let swipers = document.querySelectorAll(selector);
+    
+    swipers.forEach((swiper) => {
+        let optionsData = swiper.dataset.swiper ? JSON.parse(swiper.dataset.swiper) : {};
+
+        let options = {
+            ...defaults,
+            ...optionsData
+        };
+
+        new Swiper(swiper, options);
     });
-})
+}
