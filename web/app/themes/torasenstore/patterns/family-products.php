3edda06da5d5ee7d9ce299e3bb023f924ac72343
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2024-06-28 14:32:04 +0100

    Create family products pattern

---
+++ web/app/themes/torasenstore/patterns/family-products.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Family Products
+ * Slug: torasen/family-products
+ * Categories: gallery
+ * Description: Display list of products belonging to a given family.
+ * Keywords: family, products
+ */
+?>
+<!-- wp:torasen/family-products {"style":{"spacing":{"margin":{"top":"var:preset|spacing|xxl","bottom":"var:preset|spacing|xxl"}}}} /-->
