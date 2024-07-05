c9b4bcde616eb44a6918832c27361b99802a4b92
Adam Wilson <adam@red-fern.co.uk>
2024-07-05 14:16:03 +0100

    Add next/previous arrows to variation gallery

--- web/app/plugins/torasenstore/editor/blocks/variation-gallery/components/VariationGallery.js
+++ web/app/plugins/torasenstore/editor/blocks/variation-gallery/components/VariationGallery.js
@@ -8,7 +8,6 @@ export default function VariationGallery({}) {
 
 	const swiperParams = {
 		slidesPerView: 1.2,
-		spaceBetween: 20,
 		breakpoints: {
 			640: {
 				slidesPerView: 2,
@@ -25,16 +24,48 @@ export default function VariationGallery({}) {
 		swiperElRef.current.initialize();
 	}, []);
 
+	const nextSlide = () => {
+		swiperElRef.current.swiper.slideNext();
+		console.log(swiperElRef.current);
+	}
+
+	const prevSlide = () => {
+		swiperElRef.current.swiper.slidePrev();
+	}
+
+	const imageCount = variation?.galleryImages?.length;
+
 	return (
-		<swiper-container
-			init="false"
-			ref={swiperElRef}
-		>
-			{variation?.galleryImages?.map((mediaItem) => (
-				<swiper-slide key={mediaItem.id}>
-					<img src={mediaItem.url} alt={mediaItem.alt} key={mediaItem.id}/>
-				</swiper-slide>
-			))}
-		</swiper-container>
+		<div className="relative">
+			<div className="absolute z-20 top-0 right-0 px-8 py-8 flex items-center gap-4">
+				<button type="button" className="bg-white grid place-items-center p-3 hover:bg-[#D0D0CD]">Video</button>
+
+				{imageCount > 3 && (
+					<div className="flex divide-x divide-[#D0D0CD]">
+						<button type="button" className="bg-white grid place-items-center p-3 hover:bg-[#D0D0CD]"
+								onClick={prevSlide}>Prev
+						</button>
+						<button type="button" className="bg-white grid place-items-center p-3 hover:bg-[#D0D0CD]"
+								onClick={nextSlide}>Next
+						</button>
+					</div>
+				)}
+			</div>
+
+			<swiper-container
+				init="false"
+				ref={swiperElRef}
+				class="border-t border-b border-[#D0D0CD]"
+			>
+				{variation?.galleryImages?.map((mediaItem) => (
+					<swiper-slide
+						class="!h-auto [&:not(:first-child)]:border-l border-[#D0D0CD] p-6"
+						key={mediaItem.id}
+					>
+						<img src={mediaItem.url} alt={mediaItem.alt} key={mediaItem.id}/>
+					</swiper-slide>
+				))}
+			</swiper-container>
+		</div>
 	)
 }
