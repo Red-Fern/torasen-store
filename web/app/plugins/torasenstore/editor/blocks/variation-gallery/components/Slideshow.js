1f50025fd54a918ca9d062c98a6b6ac03e4d54ea
Adam Wilson <adam@red-fern.co.uk>
2024-07-08 09:45:26 +0100

    Style slideshow buttons

--- web/app/plugins/torasenstore/editor/blocks/variation-gallery/components/Slideshow.js
+++ web/app/plugins/torasenstore/editor/blocks/variation-gallery/components/Slideshow.js
@@ -26,7 +26,6 @@ export default function Slideshow({}) {
 
 	const nextSlide = () => {
 		swiperElRef.current.swiper.slideNext();
-		console.log(swiperElRef.current);
 	}
 
 	const prevSlide = () => {
@@ -38,15 +37,32 @@ export default function Slideshow({}) {
 	return (
 		<div className="relative">
 			<div className="absolute z-20 top-0 right-0 px-8 py-8 flex items-center gap-4">
-				<button type="button" className="bg-white grid place-items-center p-3 hover:bg-[#D0D0CD]">Video</button>
+				<button type="button" className="flex gap-2 items-center bg-white p-3 hover:bg-[#D0D0CD] shadow">
+					<span className="text-xs">Video</span>
+					<svg className="w-4 h-4 text-[#1D1D1B]" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+						<path d="M2.60156 14.4156L1.60156 15V13.8438V2.15625V1L2.60156 1.58438L12.6078 7.42188L13.6016 8L12.6078 8.57812L2.60156 14.4156ZM11.6172 8L2.60156 2.74062V13.2594L11.6172 8Z"/>
+					</svg>
+				</button>
 
 				{imageCount > 3 && (
 					<div className="flex divide-x divide-[#D0D0CD]">
-						<button type="button" className="bg-white grid place-items-center p-3 hover:bg-[#D0D0CD]"
-								onClick={prevSlide}>Prev
+						<button type="button"
+								className="bg-white grid place-items-center p-3 hover:bg-[#D0D0CD] shadow"
+								onClick={prevSlide}
+						>
+							<svg className="w-4 h-4 text-[#1D1D1B]" viewBox="0 0 16 16" fill="currentColor"
+								 xmlns="http://www.w3.org/2000/svg">
+								<path d="M1.15391 7.64683L0.800781 7.99995L1.15391 8.35308L6.40391 13.6031L6.75703 13.9562L7.46328 13.25L7.11016 12.8968L2.71328 8.49995H14.257H14.757V7.49995H14.257H2.71328L7.11016 3.10308L7.46328 2.74995L6.75703 2.0437L6.40391 2.39683L1.15391 7.64683Z"/>
+							</svg>
 						</button>
-						<button type="button" className="bg-white grid place-items-center p-3 hover:bg-[#D0D0CD]"
-								onClick={nextSlide}>Next
+						<button
+							type="button"
+							className="bg-white grid place-items-center p-3 hover:bg-[#D0D0CD] shadow"
+							onClick={nextSlide}
+						>
+							<svg className="w-4 h-4 text-[#1D1D1B]" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+								<path d="M14.4039 8.35308L14.757 7.99995L14.4039 7.64683L9.15391 2.39683L8.80078 2.0437L8.09453 2.74995L8.44766 3.10308L12.8445 7.49995H1.30078H0.800781V8.49995H1.30078H12.8445L8.44766 12.8968L8.09453 13.25L8.80078 13.9562L9.15391 13.6031L14.4039 8.35308Z"/>
+							</svg>
 						</button>
 					</div>
 				)}
