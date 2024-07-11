1b5c51e4f849bd59cd59a9b27ad638217edaf7aa
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2024-07-11 11:45:10 +0100

    Add and style additional features on log in page

--- web/app/themes/torasenstore/functions/woocommerce.php
+++ web/app/themes/torasenstore/functions/woocommerce.php
@@ -8,12 +8,11 @@ class WooCommerce
     {
         add_action('after_setup_theme', [__CLASS__, 'registerImageSizes']);
         add_filter('use_block_editor_for_post_type', [__CLASS__, 'enableBlockEditor'], 10, 2);
-
-        add_filter('woocommerce_resize_images', static function() {
-            return false;
-        });
-
+        add_filter('woocommerce_resize_images', static function() {  return false; });
         add_action('woocommerce_before_quantity_input_field', [__CLASS__, 'quantityLabel']);
+        add_filter('body_class', [__CLASS__, 'addLogInClass']);
+        add_filter('the_content', [__CLASS__, 'changeLogInTitle']);
+        add_action('woocommerce_after_customer_login_form', [__CLASS__, 'addLogInRegisterButton']);
     }
 
     public static function registerImageSizes()
@@ -37,6 +36,32 @@ class WooCommerce
     {
         ?>
         <div class="quantity-label">Quantity</div>
+        <?php
+    }
+
+    public static function addLogInClass($classes)
+    {
+        if (!is_user_logged_in() && is_account_page()) {
+            $classes[] = 'woocommerce-log-in';
+        }
+    
+        return $classes;
+    }
+    
+    public static function changeLogInTitle($text) {
+        if (!is_user_logged_in() && is_account_page()) {
+            $text = str_replace('Dealer Account', 'Dealer Login', $text);
+        }
+
+        return $text;
+    }
+
+    public static function addLogInRegisterButton() 
+    {
+        ?>
+            <div class="request-account-button wp-block-button is-style-black-outline">
+                <a class="wp-block-button__link wp-element-button" href="/contact"><?php _e( 'Request a dealer account' ); ?></a>
+            </div>
         <?php
     }
 }
