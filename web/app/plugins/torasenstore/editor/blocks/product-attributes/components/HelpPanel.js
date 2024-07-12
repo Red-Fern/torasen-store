a3014a697a5ef368e752cf37e7bdd5a3ceb82355
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2024-07-12 09:46:22 +0100

    Standardise styling of modals

--- web/app/plugins/torasenstore/editor/blocks/product-attributes/components/HelpPanel.js
+++ web/app/plugins/torasenstore/editor/blocks/product-attributes/components/HelpPanel.js
@@ -18,27 +18,31 @@ export default function HelpPanel({ title, content }) {
 			>
 				<DialogBackdrop
 					transition
-					className="fixed inset-0 bg-black/50 duration-300 ease-out data-[closed]:opacity-0"
+					className="fixed inset-0 bg-[rgba(95,95,95,.35)] duration-300 ease-out data-[closed]:opacity-0"
 				/>
 
-				<div className="fixed h-full top-0 right-0">
+				<div className="fixed inset-0 w-full h-full">
 					<DialogPanel
 						transition
-						className="max-w-lg h-full overflow-y-scroll bg-white duration-300 ease-out data-[closed]:translate-x-full"
+						className="fixed top-0 right-0 w-[480px] max-w-full h-full overflow-y-scroll bg-white duration-300 ease-out data-[closed]:translate-x-full"
 					>
 						<DialogTitle
-							className="font-bold py-6 px-12 border-b border-black flex justify-between items-center">
-							<span>{title}</span>
+							as="div"
+							className="flex items-center jusify-between gap-md px-lg py-xs border-b border-mid-grey">
+							<div className="grow font-medium">
+								<span>{title}</span>
+							</div>
 							<button
 								onClick={() => setIsOpen(false)}
-								className="p-2 bg-black"
+								className="flex items-center justify-center w-[46px] h-[46px] bg-black"
 							>
-								<svg className="w-5 h-5 text-white" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-									<path d="M8.04609 7.2937L3.90547 3.15308L3.19922 3.85933L7.33984 7.99995L3.19922 12.1406L3.90547 12.8468L8.04609 8.7062L12.1867 12.8468L12.893 12.1406L8.75234 7.99995L12.893 3.85933L12.1867 3.15308L8.04609 7.2937Z"/>
+								<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none">
+									<path fill="#fff" d="m8.046 7.294-4.14-4.14-.707.705L7.34 8 3.2 12.14l.706.707 4.141-4.14 4.14 4.14.707-.706L8.753 8l4.14-4.14-.706-.707-4.14 4.14Z"/>
 								</svg>
 							</button>
 						</DialogTitle>
-						<div className="py-8 px-12" dangerouslySetInnerHTML={{__html: content}}></div>
+
+						<div className="px-lg py-sm" dangerouslySetInnerHTML={{__html: content}}></div>
 					</DialogPanel>
 				</div>
 			</Dialog>
