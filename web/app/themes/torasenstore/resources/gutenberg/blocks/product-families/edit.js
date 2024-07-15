0b0b5a106d8bf054a75b1fc10a9957d5888ade24
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2024-07-15 15:36:31 +0100

    Add basic editor markup for product families block

--- web/app/themes/torasenstore/resources/gutenberg/blocks/product-families/edit.js
+++ web/app/themes/torasenstore/resources/gutenberg/blocks/product-families/edit.js
@@ -17,6 +17,29 @@ export default function Edit( {
 				allowedFormats={['core/bold', 'core/italic']}
 				onChange={(value) => setAttributes({ title: value })}
 			/>
+
+			<div className="mt-md border-t border-mid-grey">
+				<div className="flex flex-wrap items-center py-xs border-b border-mid-grey | lg:flex-nowrap lg:justify-end">
+					<div className="w-full flex gap-sm overflow-x-scroll | lg:flex-wrap lg:w-4/5 lg:justify-end">
+						<div className="shrink-0 w-[150px] h-[150px] bg-lightest-grey"></div>
+						<div className="shrink-0 w-[150px] h-[150px] bg-lightest-grey"></div>
+						<div className="shrink-0 w-[150px] h-[150px] bg-lightest-grey"></div>
+					</div>
+				</div>
+
+				<div className="flex flex-wrap items-center py-xs border-b border-mid-grey | lg:flex-nowrap lg:justify-end">
+					<div className="w-full flex gap-sm overflow-x-scroll | lg:flex-wrap lg:w-4/5 lg:justify-end">
+						<div className="shrink-0 w-[150px] h-[150px] bg-lightest-grey"></div>
+						<div className="shrink-0 w-[150px] h-[150px] bg-lightest-grey"></div>
+					</div>
+				</div>
+
+				<div className="flex flex-wrap items-center py-xs border-b border-mid-grey | lg:flex-nowrap lg:justify-end">
+					<div className="w-full flex gap-sm overflow-x-scroll | lg:flex-wrap lg:w-4/5 lg:justify-end">
+						<div className="shrink-0 w-[150px] h-[150px] bg-lightest-grey"></div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
