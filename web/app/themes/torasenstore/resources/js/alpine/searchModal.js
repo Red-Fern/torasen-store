9a26b318af70233a97055aa5a04332b60623698e
Elizabeth Cornforth <elizabeth@red-fern.co.uk>
2024-07-17 12:12:54 +0100

    Add cloaking to search modal and autofocus to search input

--- web/app/themes/torasenstore/resources/js/alpine/searchModal.js
+++ web/app/themes/torasenstore/resources/js/alpine/searchModal.js
@@ -9,6 +9,17 @@ export default () => ({
         this.showing = !this.showing;
 
         document.querySelector('body').classList.toggle('overflow-hidden');
+
+        // Focus the search input if the modal is shown
+        if (this.showing) {
+            this.$nextTick(() => {
+                const searchInput = document.querySelector('#woocommerce-product-search-field');
+
+                if (searchInput) {
+                    searchInput.focus();
+                }
+            });
+        }
     },
     hide() {
         this.showing = false;
