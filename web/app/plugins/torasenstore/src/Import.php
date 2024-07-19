eb727735de46de0152acde63aca51d2c7dfc5793
Alex Rowe <alex.rowe@red-fern.co.uk>
2024-07-19 19:42:42 +0100

    Final work on variation import

--- web/app/plugins/torasenstore/src/Import.php
+++ web/app/plugins/torasenstore/src/Import.php
@@ -20,13 +20,9 @@ class Import
         $this->csvPath = $uploads['basedir'] . '/product_images.csv';
     }
 
-    public function getExistingFile(string $file, $full_path = true): ?string
+    public function getExistingFile(string $file): ?string
     {
-        if ($full_path) {
-            $fileTitle = pathinfo($file, PATHINFO_FILENAME);
-        } else {
-            $fileTitle = $file;
-        }
+        $fileTitle = pathinfo($file, PATHINFO_FILENAME);
         global $wpdb;
         $query = $wpdb->prepare(
             "SELECT ID FROM $wpdb->posts WHERE post_type = 'attachment' AND post_title = %s",
@@ -96,34 +92,77 @@ class Import
 
     private function attach()
     {
-        $csv = Reader::createFromPath($this->csvPath, 'r');
-        $csv->setHeaderOffset(0);
-        $header = $csv->getHeader(); //returns the CSV header record
-        $records = $csv->getRecords();
-        foreach ($records as $record) {
-            if ($record['Parent Product SKU'] != 0) {
-                $sku = trim($record['SKU']);
-                $product_id = wc_get_product_id_by_sku($sku);
-                if ($product_id) {
-                    $images = explode(',', $record['Images']);
-                    $imageIds = [];
-                    foreach ($images as $image) {
-                        $imageId = $this->getExistingFile($image, false);
-                        if ($imageId) {
-                            $imageIds[] = $imageId;
-                        }
-                    }
-                    if (count($imageIds) == 0) {
-                        continue;
-                    }
-                    $variation = wc_get_product($product_id);
-                    // Remove legacy meta field.
-                    $variation->delete_meta_data('variation_media_gallery');
-                    usleep(10000);
-                    $variation->set_gallery_image_ids($imageIds);
-                    $variation->save();
+        try {
+            $skus = [];
+            $csv = Reader::createFromPath($this->csvPath, 'r');
+            $csv->setHeaderOffset(0);
+            $records = $csv->getRecords();
+            foreach ($records as $record) {
+                if ($record['Parent Product SKU'] != 0) {
+                    $sku = trim($record['SKU']);
+                    $skus[$sku] = $record['Images'];
                 }
             }
+
+            \WP_CLI::log("found " . count($skus) . " images to attach\n");
+
+            $statuses = array('publish', 'draft');
+            // Args on the main query for WC_Product_Query
+            $args = [
+                'status'    => $statuses,
+                'orderby'   => 'name',
+                'order'     => 'ASC',
+                'limit'     => -1,
+            ];
+
+            $vendor_products = wc_get_products($args);
+            foreach ($vendor_products as $key => $product) {
+                if ($product->get_type() == "variable") {
+                    // Args on product variations query for a variable product using a WP_Query
+                    $args2 = array(
+                        'post_parent' => $product->get_id(),
+                        'post_type'   => 'product_variation',
+                        'orderby'     => array( 'menu_order' => 'ASC', 'ID' => 'ASC' ),
+                        'fields'      => 'ids',
+                        'post_status' => $statuses,
+                        'numberposts' => -1,
+                    );
+
+                    foreach ( get_posts( $args2 ) as $child_id ) {
+                        // get an instance of the WC_Variation_product Object
+                        $variation = wc_get_product( $child_id );
+
+                        if ( ! $variation || ! $variation->exists() || !array_key_exists($variation->get_sku(),$skus) ) {
+                            \WP_CLI::warning("Variation not found for SKU: " . $product->get_sku());
+                            continue;
+                        }
+
+                        $images = explode('|', $skus[$variation->get_sku()]);
+                        $imageIds = [];
+                        foreach ($images as $image) {
+                            $imageId = $this->getExistingFile($image);
+                            if ($imageId) {
+                                $imageIds[] = $imageId;
+                            }else{
+                                \WP_CLI::warning("Image not found in media library: " . $image);
+                            }
+                        }
+                        if (count($imageIds) == 0) {
+                            \WP_CLI::warning("no images for SKU: " . $variation->get_sku());
+                            continue;
+                        }
+
+                        $variation->delete_meta_data('variation_media_gallery');
+                        usleep(10000);
+                        $variation->set_gallery_image_ids($imageIds);
+                        $variation->save();
+                    }
+                }else{
+                    //\WP_CLI::warning("Not variable product: " . $product->get_sku());
+                }
+            }
+        } catch (\Exception $e) {
+            \WP_CLI::error($e->getMessage());
         }
     }
 
@@ -136,6 +175,7 @@ class Import
         \WP_CLI::log("Starting attachment...\n");
         $this->attach();
         \WP_CLI::success("Done!\n");
+        die();
     }
 }
 
