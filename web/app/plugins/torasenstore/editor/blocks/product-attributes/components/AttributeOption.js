035c0f9ef5d86f2c5f1063a6d311ada2afa9568c
Adam Wilson <adam@red-fern.co.uk>
2024-08-07 16:24:59 +0100

    Add swatch tooltip

--- web/app/plugins/torasenstore/editor/blocks/product-attributes/components/AttributeOption.js
+++ web/app/plugins/torasenstore/editor/blocks/product-attributes/components/AttributeOption.js
@@ -1,6 +1,6 @@
-import { useState } from '@wordpress/element';
 import { ATTRIBUTE_STORE_NAME } from "../../../stores/constants";
 import { useSelect, useDispatch } from '@wordpress/data';
+import Tippy from '@tippyjs/react';
 
 export default function AttributeOption({ attribute, option }) {
 	const selectedOption = useSelect((select) => {
@@ -14,18 +14,24 @@ export default function AttributeOption({ attribute, option }) {
 		document.querySelector(`select[name="attribute_${attribute}"]`).value = option.slug;
 	}
 
-	return (
-		<button
-			type="button"
-			className={`flex items-center border border-gray-300 ${selectedOption === option.slug ? 'bg-white' : 'bg-transparent'}`}
-			onClick={() => chooseOption()}
-		>
-			{option.swatch && (
-				<span className="h-full block py-3 px-3 border-r border-gray-300 grid place-items-center">
-					<img className="w-3 h-3 object-cover" src={option.swatch} alt={option.name}/>
-				</span>
-			)}
-			<span className="block py-2 px-4">{option.name}</span>
-		</button>
-	)
+    const buttonHtml = (
+        <button
+            type="button"
+            className={`flex items-center border border-gray-300 ${selectedOption === option.slug ? 'bg-white' : 'bg-transparent'}`}
+            onClick={() => chooseOption()}
+        >
+            {option.swatch && (
+                <span className="h-full block py-3 px-3 border-r border-gray-300 grid place-items-center">
+                    <img className="w-3 h-3 object-cover" src={option.swatch} alt={option.name}/>
+                </span>
+            )}
+            <span className="block py-2 px-4">{option.name}</span>
+        </button>
+    );
+
+	return option.swatch ? (
+        <Tippy theme="light" content={<img className="w-16 h-16 object-cover" src={option.swatch} alt={option.name}/>}>
+            {buttonHtml}
+        </Tippy>
+    ) : buttonHtml;
 }
